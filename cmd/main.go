590745694c24a53e80717226f6b029baf689651c
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-01-29 10:24:54 +0700

    add swagger

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 
+	_ "github.com/brain-flowing-company/pprp-backend/docs"
+	"github.com/brain-flowing-company/pprp-backend/internal/greeting"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/swagger"
 )
 
+// @title        Bangkok Property Matchmaking Platform
+// @description  Bangkok Property Matchmaking Platform API docs
+// @version      1.0
+// @host         localhost:3000
+// @BasePath     /
 func main() {
 	app := fiber.New()
 
@@ -15,11 +23,14 @@ func main() {
 		TimeZone:   "Asia/Bangkok",
 	}))
 
-	app.Get("/greeting", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Worldgo")
-	})
+	app.Get("/docs/*", swagger.HandlerDefault)
 
-	err := app.Listen(fmt.Sprintf(":3000"))
+	hwService := greeting.NewService()
+	hwHandler := greeting.NewHandler(hwService)
+
+	app.Get("/greeting", hwHandler.Greeting)
+
+	err := app.Listen(":3000")
 	if err != nil {
 		panic(fmt.Sprintf("Server cannot start with error: %v", err.Error()))
 	}
